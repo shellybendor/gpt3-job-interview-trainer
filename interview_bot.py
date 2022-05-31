2e0f2e506d4529c681ab92b4c0e382a20c4e67ff
shelly.bendor <shelly.bendor@cs.huji.ac.il>
2022-05-31 18:05:45 +0300

    added finetuning but now responses are sometime empty

--- interview_bot.py
+++ interview_bot.py
@@ -29,7 +29,24 @@ def get_initial_question():
         presence_penalty=0.6,
         stop=[START_SEQUENCE, RESTART_SEQUENCE, "\n"]
       )
+      # import ipdb; ipdb.set_trace()
       return response.choices[0].text.strip()
+
+
+def initial_finetune():
+      with open("./fine_tuning.txt", "r") as file:
+        response = openai.Completion.create(
+          engine="text-davinci-002",
+          prompt=file.read()+INITIAL_PROMPT+INITIAL_CHAT_LOG+START_SEQUENCE,
+          temperature=0.9,
+          max_tokens=150,
+          top_p=1,
+          frequency_penalty=0,
+          presence_penalty=0.6,
+          stop=[START_SEQUENCE, RESTART_SEQUENCE, "\n"]
+        )
+        return response.choices[0].text.strip()
+
 
 def respond(chat_log):
       response = openai.Completion.create(
@@ -42,6 +59,7 @@ def respond(chat_log):
         presence_penalty=0.6,
         stop=[START_SEQUENCE, RESTART_SEQUENCE, "\n"]
       )
+      # import ipdb; ipdb.set_trace()
       return response.choices[0].text.strip()
 
 
